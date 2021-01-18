e44596115be9ed82d439bc253562c96122535e29
DaniyarBM <donya9bd@gmail.com>
2021-01-19 00:54:58 +0600

    copied models.py and views.py

--- main/views.py
+++ main/views.py
@@ -10,7 +10,8 @@ def test(request):
     return render(request, "test.html", {"todo_list": todo_list})
 
 def second(request):
-    return HttpResponse("test 2 page")
+    # return HttpResponse("test 2 page")
+    return render(request, "books.html")
 
 def third(request):
     return HttpResponse("This is page test3")
