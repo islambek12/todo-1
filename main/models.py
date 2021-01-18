cf16f9ce36d8255e7dc0a6c8db451d65de9413d6
DaniyarBM <donya9bd@gmail.com>
2021-01-19 02:31:43 +0600

    copied models.py and books.html

--- main/models.py
+++ main/models.py
@@ -26,32 +26,32 @@ class Book(models.Model):
     # isbn = models.CharField('ISBN',max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
 
-class BookInstance(models.Model):
-    """
-    Model representing a specific copy of a book (i.e. that can be borrowed from the library).
-    """
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular book across whole library")
-    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
-    imprint = models.CharField(max_length=200)
-    due_back = models.DateField(null=True, blank=True)
+# class BookInstance(models.Model):
+#     """
+#     Model representing a specific copy of a book (i.e. that can be borrowed from the library).
+#     """
+#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular book across whole library")
+#     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+#     imprint = models.CharField(max_length=200)
+#     due_back = models.DateField(null=True, blank=True)
 
-    LOAN_STATUS = (
-        ('m', 'Maintenance'),
-        ('o', 'On loan'),
-        ('a', 'Available'),
-        ('r', 'Reserved'),
-    )
+#     LOAN_STATUS = (
+#         ('m', 'Maintenance'),
+#         ('o', 'On loan'),
+#         ('a', 'Available'),
+#         ('r', 'Reserved'),
+#     )
 
-    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text='Book availability')
+#     status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text='Book availability')
 
-    class Meta:
-        ordering = ["due_back"]
+#     class Meta:
+#         ordering = ["due_back"]
 
-class Author(models.Model):
-    """
-    Model representing an author.
-    """
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    date_of_birth = models.DateField(null=True, blank=True)
-    date_of_death = models.DateField('Died', null=True, blank=True)
+# class Author(models.Model):
+#     """
+#     Model representing an author.
+#     """
+#     first_name = models.CharField(max_length=100)
+#     last_name = models.CharField(max_length=100)
+#     date_of_birth = models.DateField(null=True, blank=True)
+#     date_of_death = models.DateField('Died', null=True, blank=True)
