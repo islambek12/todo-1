26b0a8552b78f48b84227b9ab57121a66ca38970
DaniyarBM <donya9bd@gmail.com>
2021-01-19 14:35:07 +0600

    copied urls

--- todo/urls.py
+++ todo/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import homepage, test, second, third
+from main.views import homepage, test, second, third, booksShop
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -26,7 +26,7 @@ urlpatterns = [
     path("test/", test, name="test"),
     path("test2/", second),
     path("test3/", third),
-    path('booksShop/', BooksShop)
+    path('booksShop/', booksShop)
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
