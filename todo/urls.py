fe74e1a9599840d3e074dda6fe28207fd12da176
DaniyarBM <donya9bd@gmail.com>
2021-01-19 14:28:22 +0600

    added urls

--- todo/urls.py
+++ todo/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path("test/", test, name="test"),
     path("test2/", second),
     path("test3/", third),
-    path('booksShop/', booksShop)
+    path('booksShop/', BooksShop)
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
