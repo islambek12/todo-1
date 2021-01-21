2e204e031073fb75821581f442340fb9f734f3b3
DaniyarBM <donya9bd@gmail.com>
2021-01-21 17:02:48 +0600

    copied admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import ToDo
-from .models import ToDo, BooksShop
+from .models import ToDo, BookShop
 
 
 
 admin.site.register(ToDo)
-admin.site.register(BooksShop)
+admin.site.register(BookShop)
 
